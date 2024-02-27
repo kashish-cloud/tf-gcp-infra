71077edaa08d07876adff2217a92d2678fa4bd95
Kashish Desai <113315022+kashishdesai03@users.noreply.github.com>
2024-02-27 10:23:21 -0500

    added instance

--- main.tf
+++ main.tf
@@ -5,9 +5,9 @@ provider "google" {
 }
 
 resource "google_compute_network" "my_vpc" {
-  name                    = "my-vpc"
-  auto_create_subnetworks = false
-  routing_mode            = "REGIONAL"
+  name                            = "my-vpc"
+  auto_create_subnetworks         = false
+  routing_mode                    = "REGIONAL"
   delete_default_routes_on_create = true
 }
 
@@ -24,11 +24,11 @@ resource "google_compute_subnetwork" "db_subnet" {
 }
 
 resource "google_compute_route" "route_webapp_subnet" {
-  name                    = "route-webapp-subnet"
-  network                 = google_compute_network.my_vpc.self_link
-  dest_range              = "0.0.0.0/0"
-  next_hop_gateway        = "default-internet-gateway"
-  priority                = 1000
+  name             = "route-webapp-subnet"
+  network          = google_compute_network.my_vpc.self_link
+  dest_range       = "0.0.0.0/0"
+  next_hop_gateway = "default-internet-gateway"
+  priority         = 1000
 
   depends_on = [google_compute_network.my_vpc]
 }
@@ -39,39 +39,45 @@ resource "google_compute_firewall" "app_firewall" {
 
   allow {
     protocol = "tcp"
-    ports    = [8080]
+    ports    = [8080, 22]
   }
 
   source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["webapp"]
+  target_tags   = ["app"]
 }
 
 resource "google_compute_disk" "app_disk" {
-  name  = "app-disk"
-  size  = 100
-  type  = "pd-standard"
-  zone  = "us-east1-b"  # Replace with your desired zone
+  name = "app-disk"
+  size = 100
+  type = "pd-standard"
+  zone = "us-east1-b"
 }
 
-resource "google_compute_image" "custom_app_image" {
-  name         = "custom-app-image"
-  source_disk  = google_compute_disk.app_disk.id
-  project      = "tf-gcp-infra-project"
-}
+/*resource "google_compute_image" "custom_app_image" {
+  name        = "packer-1708816833"
+  source_disk = google_compute_disk.app_disk.id
+  project     = "tf-gcp-infra-project"
+}*/
 
 resource "google_compute_instance" "app_instance" {
   name         = "app-instance"
   machine_type = "n1-standard-1"
   zone         = "us-east1-b"
+  tags         = ["app"]
 
   boot_disk {
     initialize_params {
-      image = google_compute_image.custom_app_image.self_link
+      image = "projects/tf-gcp-infra-project/global/images/packer-1709001176"
+      size  = "100"
+      type  = "pd-balanced"
     }
   }
 
   network_interface {
-    network = google_compute_network.my_vpc.self_link
+    network    = google_compute_network.my_vpc.self_link
     subnetwork = google_compute_subnetwork.webapp_subnet.self_link
+    access_config {
+
+    }
   }
 }

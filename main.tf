485dabaaf31bfed4662010a91720de52d521c6ce
Kashish Desai <113315022+kashishdesai03@users.noreply.github.com>
2024-03-28 17:53:35 -0400

    added code

--- main.tf
+++ main.tf
@@ -142,6 +142,22 @@ resource "google_project_iam_binding" "ops_agent_monitoring_binding" {
   ]
 }
 
+# Creating a new service account for Pub/Sub
+resource "google_service_account" "pubsub_service_account" {
+  account_id   = "pubsub-service-account"
+  display_name = "Pub/Sub Service Account"
+  project      = "tf-gcp-infra-project"
+}
+
+resource "google_project_iam_binding" "pubsub_publisher_binding" {
+  project = "tf-gcp-infra-project"
+  role    = "roles/pubsub.publisher"
+
+  members = [
+    "serviceAccount:${google_service_account.pubsub_service_account.email}"
+  ]
+}
+
 resource "google_compute_instance" "app_instance" {
   name         = "app-instance"
   machine_type = "n1-standard-1"
@@ -150,7 +166,7 @@ resource "google_compute_instance" "app_instance" {
 
   boot_disk {
     initialize_params {
-      image = "projects/tf-gcp-infra-project/global/images/packer-1711060281"
+      image = "projects/tf-gcp-infra-project/global/images/packer-1711657940"
       size  = "100"
       type  = "pd-balanced"
     }
@@ -178,7 +194,7 @@ resource "google_compute_instance" "app_instance" {
     echo "DBUSER=${google_sql_user.cloudsql_user.name}" >> /opt/webapp/.env
     echo "DBPASSWORD=${random_password.database_password.result}" >> /opt/webapp/.env
     systemctl start kas.service
-  EOF
+    EOF
 }
 
 resource "google_dns_record_set" "example" {
@@ -190,4 +206,110 @@ resource "google_dns_record_set" "example" {
   rrdatas = [
     google_compute_instance.app_instance.network_interface.0.access_config.0.nat_ip,
   ]
+}
+
+// Create Pub/Sub topic for email verification
+resource "google_pubsub_topic" "verify_email" {
+  name = "verify-email"
+}
+
+// Create subscription for the Cloud Function
+resource "google_pubsub_subscription" "verify_email_subscription" {
+  name                 = "verify-email-subscription"
+  topic                = google_pubsub_topic.verify_email.name
+  ack_deadline_seconds = 10
+
+  expiration_policy {
+    ttl = "604800s" // 7 days in seconds
+  }
+}
+
+// Bind IAM roles for Cloud Function
+resource "google_project_iam_binding" "cloud_function_invoker_binding" {
+  project = "tf-gcp-infra-project"
+  role    = "roles/cloudfunctions.invoker"
+
+  members = [
+    "serviceAccount:${google_service_account.pubsub_service_account.email}"
+  ]
+}
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.34.0"
+    }
+  }
+}
+
+resource "random_id" "bucket_prefix" {
+  byte_length = 8
+}
+
+resource "google_storage_bucket" "serverless_bucket" {
+  name                        = "${random_id.bucket_prefix.hex}-gcf-source"
+  location                    = "US"
+  uniform_bucket_level_access = true
+}
+
+/*data "archive_file" "serverless_code" {
+  type        = "zip"
+  output_path = "/tmp/serverless.zip"
+  source_dir  = "./serverless"
+}*/
+
+resource "google_storage_bucket_object" "serverless_code" {
+  name   = "serverless.zip"
+  bucket = google_storage_bucket.serverless_bucket.name
+  source = "serverless.zip"
+}
+
+resource "google_service_networking_connection" "vpc_connector" {
+  network                 = google_compute_network.my_vpc.self_link
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_service_ip_range.name]
+}
+
+// Deploy Cloud Function
+resource "google_cloudfunctions2_function" "cloud_function" {
+  name        = "verify-email-function"
+  location    = "us-east1"
+  description = "A new function"
+
+  build_config {
+    runtime     = "nodejs16"
+    entry_point = "verifyEmailFunction"
+    source {
+      storage_source {
+        bucket = google_storage_bucket.serverless_bucket.name
+        object = google_storage_bucket_object.serverless_code.name
+      }
+    }
+  }
+
+  service_config {
+    max_instance_count = 3
+    min_instance_count = 1
+    available_memory   = "256M"
+    timeout_seconds    = 60
+    environment_variables = {
+      DB_HOST     = google_sql_database_instance.cloudsql_instance.ip_address[0].ip_address
+      DB_PORT     = "5432"
+      DB_NAME     = google_sql_database.cloudsql_database.name
+      DB_USER     = google_sql_user.cloudsql_user.name
+      DB_PASSWORD = random_password.database_password.result
+    }
+    ingress_settings               = "ALLOW_INTERNAL_ONLY"
+    all_traffic_on_latest_revision = true
+    vpc_connector                  = google_service_networking_connection.vpc_connector.id
+    service_account_email          = google_service_account.pubsub_service_account.email
+  }
+
+  event_trigger {
+    trigger_region = "us-east1"
+    event_type     = "google.cloud.pubsub.topic.v1.messagePublished"
+    pubsub_topic   = google_pubsub_topic.verify_email.id
+    retry_policy   = "RETRY_POLICY_RETRY"
+  }
 }
